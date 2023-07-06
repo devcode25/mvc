91641f0779bdcc64875bf3694bdb41212ec5b9e5
devcode25 <tereza.dev@outlook.com>
2023-07-06 17:11:28 -0300

    Created post method

--- src/controllers/apiControllers.js
+++ src/controllers/apiControllers.js
@@ -6,7 +6,19 @@ class ApiController {
         apis.find((err, apis) => {
             res.status(200).json(apis)
         })
-    }      
+    }
+    
+    static createApis = (req, res) => {
+        let api = new apis(req.body);
+      
+        api.save((err) => {
+          if (err) {
+            res.status(500).send({ message: `${err.message} - falha ao cadastrar api` })
+          } else {
+            res.status(201).send(api.toJSON())
+          }
+        })
+    }
 }
 
 export default ApiController;
--- src/routes/api.js
+++ src/routes/api.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 router
 .get("/", ApiController.getAllApis)
+.post("/", ApiController.createApis)
 
-export default router
+
+export default router;
 
